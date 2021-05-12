17e2bce3e257cd0b092102cbe969eadc55b097e1
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2021-05-13 03:44:00 +0530

    added test for redis & close conection, fix codecy

--- test/database.spec.js
+++ test/database.spec.js
@@ -23,18 +23,30 @@ describe('Test PostgreSQL database connection', () => {
     });
 
     it('Should Successfully register User', async () => {
-        const results = await registerUser('omkaragrawal', 1, 'omkar@omkar.omkar', 'omkar', '1234567890', 'https://google.com/imghp');
+        const results = await registerUser('testUser', 1, 'test@test.test', 'test', '1234567890', 'https://google.com/imghp');
 
-        let deleteResult = await postgreDatabase.query("delete from user_table where user_name = 'omkaragrawal' returning *;");
+        let deleteResult = await postgreDatabase.query("delete from user_table where user_name = 'testUser' returning *;");
 
         let alterSequenceResult = await postgreDatabase.query(`Select setval('user_table_id_seq', ${results.rows[0].id - 1}, true);`);
 
-        
+        await postgreDatabase.end();
+        console.log('pool has drained');
         expect(results).not.be.empty;
         expect(deleteResult).not.be.empty;
         expect(alterSequenceResult).not.be.empty;
         expect(`${results.rows[0].id - 1}`).to.be.equal(alterSequenceResult.rows[0].setval);
         
     });
-
 });
+
+describe('Test Redis', () => {
+    it('Test Redis Database', async () => {
+        await redisDatabase.set('abcd', 1234)
+        let value = await redisDatabase.get("abcd");
+        await redisDatabase.del('abcd');
+        await redisDatabase.disconnect(false);
+        console.log('Redis disconnected');
+        expect(value).to.be.equal('1234');
+
+    });
+})
