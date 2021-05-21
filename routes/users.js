c808561047c4076536e62c467cb80e012faee15f
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2021-05-21 14:51:46 +0530

    Update routes/users.js
    Co-authored-by: OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -57,7 +57,7 @@ function checkLogin(req, res, next) {
 // router.use('/admin', (req, _, next) => {req.allowedUserType = "admin"; req.differentUserType = "student"; next();}, checkLogin, adminRouter);
 // router.use('/student', (req, _, next) => {req.allowedUserType = "student"; req.differentUserType = "admin"; next();}, checkLogin, studentRouter);
 
-router.use('/superAdmin',(req, _, next) => {req.allowedUserType = "superAdmin";next();},checkLogin,superadminRouter)
+router.use('/super-admin',(req, _, next) => {req.allowedUserType = "superAdmin";next();},checkLogin,superadminRouter)
 router.use('/subAdmin',(req, _, next) => {req.allowedUserType = "subAdmin";next();},checkLogin, subadminRouter);
 router.use('/mentor',(req, _, next) => {req.allowedUserType = "mentor";next();},checkLogin,mentorRouter)
 router.use('/student',(req, _, next) => {req.allowedUserType = "student";next();},checkLogin, studentRouter);
