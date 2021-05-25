cffc2476126d954a5d3b6b7a0ff7cf3fd4759fd8
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2021-05-25 11:09:54 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const debug = require('debug')('abc:server:index.js');
