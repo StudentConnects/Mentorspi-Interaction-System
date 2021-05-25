1e341fb882f1ff8b94a47ef9fbf18d80e54cd9d9
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2021-05-25 09:53:33 +0530

    students and Mentors adden

--- public/assets/js/mentorchatdata.js
+++ public/assets/js/mentorchatdata.js
@@ -37,3 +37,43 @@ document.addEventListener(
     },
     false
   );
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      fetch('/users/mentor/studentdata', {
+        method: "GET",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) =>
+          response.json().then((text) => {
+            if (response.ok) {
+               console.log(text) 
+               let name1 = document.getElementById('student_list')
+               var list ="";
+               text.forEach(element => {
+                 console.log(element)
+                var divtag =`<div class="intro-x cursor-pointer box relative flex items-center p-5 ">
+                <div class="w-12 h-12 flex-none image-fit mr-1">
+                    <img class="rounded-full" src="../../assets/img/user.png">
+                </div>
+                <div class="ml-2 overflow-hidden">
+                    <div class="flex items-center">
+                        <a href="javascript:;" class="font-medium">${element.user_name}</a>
+                        
+                    </div>
+                    <div class="w-full truncate text-gray-600">${element.organization}</div>
+                </div>
+            </div>`
+                list +=divtag
+               });
+               name1.innerHTML = list
+            }
+            return response.status;
+          })
+        )
+        .then((json) => console.log(json))
+        .catch((err) => console.log(err));
+    },
+    false
+  );
